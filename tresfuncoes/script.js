7787552e2ce42e9914c081ab3356b6ea7e22b948
Javan de França <110273437+JavanFA@users.noreply.github.com>
2023-11-20 15:30:42 -0300

    add files

---
+++ tresfuncoes/script.js
@@ -0,0 +1,17 @@
+function saudacao() {
+    console.log("Olá! Bem-vindo à calculadora.");
+}
+
+function somar(a, b) {
+    return a + b;
+}
+
+const multiplicar = (a, b) => a * b;
+
+saudacao();  
+    var resultadoSoma = somar(5, 3);  
+    var resultadoMultiplicacao = multiplicar(4, 2);  
+
+console.log("Resultado da soma:", resultadoSoma);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+
